37c1684cd61845aa798da423c13c8f54568fe3e0
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-03-11 21:36:55 +0530

    db setup complete

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 const Page = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <>
-      <div>test page</div>
+      <div>
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id}>{text}</div>
+        ))}
+      </div>
     </>
   );
 };
