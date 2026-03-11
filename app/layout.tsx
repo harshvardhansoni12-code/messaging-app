0cd2bf3e79fe13f610b265fd5420595b30472fa7
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-03-11 21:29:37 +0530

    addeddb

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import { ConvexClientProvider } from "../components/ConvexClientProvider";
 const inter = Inter({ subsets: ["latin"] });
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
 import { AuthScreen } from "@/features/auth/components/auth-screen";
 export default function Home() {
   return (
     <>
       <div className="h-screen w-full flex items-center justify-center bg-[#5C3B58]">
-        <div className="md:h-auto md:w-90">
-          <AuthScreen />
-        </div>
+        <div className="md:h-auto md:w-90"></div>
       </div>
     </>
   );
