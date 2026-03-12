8fd5d87b1cbe249c1f8813943ae45118be5d1fbc
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-03-12 18:44:22 +0530

    db set up

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <>
       <div className="h-screen w-full flex items-center justify-center bg-[#5C3B58]">
-        <div className="md:h-auto md:w-90"></div>
+        <div className="md:h-auto md:w-90">
+          <AuthScreen />
+        </div>
       </div>
     </>
   );
